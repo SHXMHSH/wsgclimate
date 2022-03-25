465d4db7956348548f3aed6658e96e201a64317f
Krzysztof Świderski <Shemhashi@outlook.com>
2022-03-26 00:50:31 +0100

    Dodanie zarzadzania

---
+++ wsgclimate/Controllers/ManagementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace wsgclimate.Controllers
+{
+    public class ManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
