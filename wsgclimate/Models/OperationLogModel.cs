a081a4a3873ab17f10283448c54d055bc984f339
Krzysztof Świderski <Shemhashi@outlook.com>
2022-03-26 01:48:12 +0100

    dodanie modeli

---
+++ wsgclimate/Models/OperationLogModel.cs
@@ -0,0 +1,9 @@
+﻿namespace wsgclimate.Models
+{
+    public class OperationLogModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ wsgclimate/Models/RoomModel.cs
@@ -0,0 +1,14 @@
+﻿namespace wsgclimate.Models
+{
+    public class RoomModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int SeatedPlaces { get; set; }
+        public decimal SquareMeters { get; set; }
+        public decimal MaxTemperature { get; set; }
+        public decimal MaxHumidity { get; set; }
+
+
+    }
+}
